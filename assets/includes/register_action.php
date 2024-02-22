56c12f9b0a53925d2ff0d805f4129a4418808dfa
Dev  Mode <dev-mode@MacBook-Pro-4.local>
2024-02-22 22:00:35 +0600

    add regex and update registration

--- assets/includes/register_action.php
+++ assets/includes/register_action.php
@@ -24,6 +24,9 @@ if(isset($_POST['register_student'])){
     $student_password = real_escape($_POST['student_password']);
     $student_cfpassword = real_escape($_POST['student_cfpassword']);
 
+    $phone_rgex = "/(\+88)?-?01[3-9]\d{8}/";
+    $email_regx = "/([a-z\d][a-z\d_\-.]+[a-z\d]){1,10}@lus.ac.bd/";
+
     // File upload handling
     $student_avater = $_FILES['student_avater']['name'];
     $student_avater_loc = $_FILES['student_avater']['tmp_name'];
@@ -40,48 +43,86 @@ if(isset($_POST['register_student'])){
         // Generate verification code
         $verification_code = bin2hex(random_bytes(16));
 
-        // Instantiate PHPMailer
-        $mail = new PHPMailer();
 
-        try {
-            // SMTP configuration
-            $mail->SMTPDebug = 0;
-            $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com';
-            $mail->SMTPAuth = true;
-            $mail->Username = 'algorithmsunlocks@gmail.com'; // Your Gmail email address
-            $mail->Password = 'kkvk htcc tdbn ixvn'; // Your Gmail password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            $mail->Port = 587;
+        if(!empty($student_name) && !empty($student_id) && !empty($student_password)){
 
-            // Set email parameters
-            $mail->setFrom('algorithmsunlocks@gmail.com', 'Algo Unlocks');
-            $mail->addAddress($student_email);
-            $mail->isHTML(true);
-            $mail->Subject = 'Email Verification';
-            $mail->Body = "Your verification code is: $verification_code";
+            if(!preg_match($phone_rgex,$student_phone)){
 
-            // Send email
-            $mail->send();
+                echo "<script>
+                        alert('Phone number is not accepted');
+                        window.location.href = '../../register.php';
+                  </script>";
 
-            // Insert user data into database
-            $query = "INSERT INTO `users`(`name`, `email`, `phone`, `st_id`, `department`, `dob`, `blood_group`, `password`, `profile`, `verification_code`, `is_verify`) VALUES ('$student_name','$student_email','$student_phone','$student_id','$student_department','$student_dob','$user_blood_group','$student_password','$avatar_path','$verification_code','0')";
-            $result = mysqli_query($connection, $query);
+            }else if(mysqli_num_rows($duplicate_student_id)){
 
-            if($result) {
-                // Redirect to verification page if insertion was successful
-                header("Location: verify-code.php?email=$student_email");
-                exit();
-            } else {
-                echo "Error: " . mysqli_error($connection); // Print MySQL error message if insertion fails
-            }
+                echo "<script> alert('Student id is already used '); window.location.href = '../../register.php'; </script>";
 
-        } catch (Exception $e) {
-            // Handle errors
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }else if(mysqli_num_rows($duplicate_student_email)){
+
+                echo "<script> alert('Student email is already used'); window.location.href = '../../register.php'; </script>";
+
+            }else if(mysqli_num_rows($duplicate_student_phone)){
+
+                echo "<script> alert('Student phone number is already used'); window.location.href = '../../register.php';</script>";
+
+            }else
+                if($student_password !== $student_cfpassword){
+
+                    echo "<script> alert('Password and Confirm password is not match');window.location.href = '../../register.php'; </script>";
+
+                }
+                else if(!preg_match($email_regx,$student_email)){
+
+                    echo "<script> alert('Email is not valid'); window.location.href = '../../register.php';</script>";
+
+                }
+                else{
+                    // Instantiate PHPMailer
+                    $mail = new PHPMailer();
+
+                    try {
+                        // SMTP configuration
+                        $mail->SMTPDebug = 0;
+                        $mail->isSMTP();
+                        $mail->Host = 'smtp.gmail.com';
+                        $mail->SMTPAuth = true;
+                        $mail->Username = 'lu.aide12345@gmail.com';
+                        $mail->Password = 'sjps zpin pklp rucb';
+                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                        $mail->Port = 587;
+
+                        $mail->setFrom('lu.aide12345@gmail.com', 'LU AID');
+                        $mail->addAddress($student_email);
+                        $mail->isHTML(true);
+                        $mail->Subject = 'Email Verification';
+                        $mail->Body = "Thanks for your registration; your verification code is: $verification_code";
+
+
+                        $mail->send();
+
+                        $query = "INSERT INTO `users`(`name`, `email`, `phone`, `st_id`, `department`, `dob`, `blood_group`, `password`, `profile`, `verification_code`, `is_verify`) VALUES ('$student_name','$student_email','$student_phone','$student_id','$student_department','$student_dob','$user_blood_group','$student_password','$avatar_path','$verification_code','0')";
+                        $result = mysqli_query($connection, $query);
+
+                        if($result) {
+                            header("Location: verify-code.php?email=$student_email");
+                            exit();
+                        } else {
+                            echo "Error: " . mysqli_error($connection);
+                        }
+
+                    } catch (Exception $e) {
+                        // Handle errors
+                        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                    }
+                }
+
+        }else{
+
+            echo "<script> alert('Data is empty'); window.location.href = '../../register.php'; </script>";
+
         }
+
     } else {
-        echo "Failed to upload avatar."; // Print error message if file upload fails
+        echo "Failed to upload avatar.";
     }
 }
-?>
